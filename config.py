41ce0a0683359728f94431eb5639826b7fac9197
hoangdat77ct <66303132+hoangdat77ct@users.noreply.github.com>
2022-05-16 14:24:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,3 @@ bucket ="vis-search-demo-visearchs3-10tctx18l33jv"
 region= "us-east-1" #AWS_DEFAULT_REGION
 es_host = "search-vis-sea-domain-1e7jdgl81yrte-hb7kkkxuu444e6h3ftpnw3vfq4.us-east-1.es.amazonaws.com"
 sm_endpoint = "tensorflow-inference-2022-05-16-01-22-13-806"
-access_key="AKIA2C6OHXSGJ7B2JWOE" #AWS_ACCESS_KEY_ID
-secret_key="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" #AWS_SECRET_ACCESS_KEY
-session_token="IQoJb3JpZ2luX2VjEMH//////////wEaCXVzLWVhc3QtMSJHMEUCIQDWneOEk4E7vqvg+nlVIHNmP86ftrkV91z4SuHqYl9hmAIgDwIpawSQrizC6wI3ba3AbkaNNzMKIQNahOJUL9XottsqvQIImv//////////ARAAGgw2OTM1MzI4Njc3MjQiDA/QlJw0aAwe7Zsr0CqRAlwjdZxZbngIEEQTyjCDH3oDf/oHt+TTK8sD/efqep6nOvJ3FxnqZs3gNMhNkBSlPPXpOP9GtRo2CnsRFPFTiNDX53IVUjq2btjjc4AMBA9neQOCYxX8591GCkehiZAmG9XmmybsqZKtmbfFqUFGeY8HYnJBgrtiBezzybiagK7iH2RWCuEMWFquvYTHPF29QC2QYw/sLqpEUmndKg9L5qdJAYLKM/83XzBZoKWEh4LBTMr3AbolpEbvQ5OfvBGZvFt6y3yDyVudJPA0O/yHvcbIqk/lAQYWK05MBcm8C8a/AST1YD3cgwsDz97Xxxu8D/iU/B5OEJC/eILaSa/u1uf8SoA+dgbfrSf/EYje2/5uojCHxYaUBjqTAdxxhsidkoiZmkn/VHoAau+Qmz5By+g3wUK+YGUf8G8u5+k9cX0JlJhaiuFsYU5k20YRGnFdCBpbXnR2tKS9xtdfCaBd3qcpfMc/z9GRC1KJND+4siiTDlc5y3U6fk3kxraYOlZSANpg5P6ltjasJ5Ps+hDbu8uU7GXZCKvoLzMbN7a4b/DG0muhdOxG8fAeQU2Ndg=="
